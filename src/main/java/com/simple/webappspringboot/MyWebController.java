7ec279ca17b50b25bd23fe6e2852909deac2fa10
eliyas_alam <eliyas.alam.work@gmail.com>
2024-05-17 11:14:33 +0530

    Initial commit for simple web app using thymeleaf--created static home page and a dynamic page

---
+++ src/main/java/com/simple/webappspringboot/MyWebController.java
@@ -0,0 +1,40 @@
+package com.simple.webappspringboot;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+
+@Controller
+@RequestMapping("/web-app")
+public class MyWebController {
+
+    @GetMapping("/home")
+    public ResponseEntity<? extends Object> getHomePage() {
+        try {
+            Resource resource = new ClassPathResource("static/index.html");
+            return ResponseEntity.ok().body(resource);
+        } catch (Exception e) {
+            // Handle error if the file is not found or other exceptions
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/dynamic")
+    public String dynamicPage(@RequestParam(name = "show", required = false) boolean show, Model model) {
+        model.addAttribute("features", Arrays.asList(
+                "ABC",
+                "DEF",
+                "GHI"
+        ));
+        model.addAttribute("show", show);
+        return "dynamic-page";
+    }
+
+}
